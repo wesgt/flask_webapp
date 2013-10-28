22ef5ed88460b3929e85073f0f78b6875ab4cb01
wes <wes@softstar.com.tw>
2013-10-28 14:09:46 +0800

    Refactor quickstart : 提出 copyfile

--- flask_webapp/quickstart.py
+++ flask_webapp/quickstart.py
@@ -98,25 +98,21 @@ def generate(d, overwrite=True, silent=False):
 
     def write_file(fpath, content, newline=None):
         if overwrite or not path.isfile(fpath):
-            print('Creating file {0}.'.format(fpath))
+            print('Writing file {0}.'.format(fpath))
             with open(fpath, 'at', encoding='utf-8', newline=newline) as f:
                 f.write(content)
         else:
             print('File {0} already exists, skipping.'.format(fpath))
 
-    here = path.dirname(path.abspath(__file__))
+    def copy_file(fpath):
+        print('Creating file {0}.'.format(fpath))
+        here = path.dirname(path.abspath(__file__))
+        shutil.copyfile(path.join(here, fpath), path.join(webapp_root_path, fpath))
 
-    shutil.copyfile(path.join(here, 'webapp_main.py'),
-                    path.join(webapp_root_path, 'webapp_main.py'))
-
-    shutil.copyfile(path.join(here, 'webapp/__init__.py'),
-                    path.join(webapp_root_path, 'webapp/__init__.py'))
-
-    shutil.copyfile(path.join(here, 'tests/__init__.py'),
-                    path.join(webapp_root_path, 'tests/__init__.py'))
-
-    shutil.copyfile(path.join(here, 'tests/run.py'),
-                    path.join(webapp_root_path, 'tests/run.py'))
+    copy_file('webapp_main.py')
+    copy_file('webapp/__init__.py')
+    copy_file('tests/__init__.py')
+    copy_file('tests/run.py')
 
     config_content = 'DEBUG = True\n' + "SERVER_IP = '172.18.102.104'\n" + \
         'SERVER_PORT = 80\n' + "LOG_PATH = 'log/webapp.log'\n" + \
@@ -127,27 +123,23 @@ def generate(d, overwrite=True, silent=False):
 
     # add iap module
     if d['open_iap']:
-        shutil.copyfile(path.join(here, 'webapp/in_app_purchase.py'),
-                    path.join(webapp_root_path, 'webapp/in_app_purchase.py'))
+        copy_file('webapp/in_app_purchase.py')
 
         iap_blueprint_contant = 'from webapp.in_app_purchase import iap\n' + \
                                 "app.register_blueprint(iap, url_prefix='/iap')\n"
         write_file(path.join(webapp_root_path, 'webapp/__init__.py'), iap_blueprint_contant)
 
-        shutil.copyfile(path.join(here, 'tests/test_in_app_purchase.py'),
-                    path.join(webapp_root_path, 'tests/test_in_app_purchase.py'))
+        copy_file('tests/test_in_app_purchase.py')
 
     # add push notification module
     if d['open_push_notification']:
-        shutil.copyfile(path.join(here, 'webapp/push_notification.py'),
-                    path.join(webapp_root_path, 'webapp/push_notification.py'))
+        copy_file('webapp/push_notification.py')
 
         push_blueprint_contant = 'from webapp.push_notification import push_notification\n' + \
                     "app.register_blueprint(push_notification, url_prefix='/push_notification')\n"
         write_file(path.join(webapp_root_path, 'webapp/__init__.py'), push_blueprint_contant)
 
-        shutil.copyfile(path.join(here, 'tests/test_push_notification.py'),
-                    path.join(webapp_root_path, 'tests/test_push_notification.py'))
+        copy_file('tests/test_push_notification.py')
 
     print()
     print('Finished: An new webapp has been created.')
