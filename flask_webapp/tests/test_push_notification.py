b03967c05b6851e4cb3a83fbc2725bb69ead68f0
wes <wes@softstar.com.tw>
2013-10-29 15:08:56 +0800

    Add register devices token test case

--- flask_webapp/tests/test_push_notification.py
+++ flask_webapp/tests/test_push_notification.py
@@ -1,7 +1,7 @@
 import unittest
 from flask import json
 import webapp
-from webapp.push_notification import push_notification
+from webapp.push_notification import push_notification, ResultType
 
 
 class PushNotificationTestCase(unittest.TestCase):
@@ -16,11 +16,35 @@ class PushNotificationTestCase(unittest.TestCase):
     def setUpClass():
         webapp.app = webapp.create_app('config.cfg')
         webapp.app.config['TESTING'] = True
-        webapp.app.register_blueprint(push_notification, url_prefix='/push_notification')
+        webapp.app.register_blueprint(
+            push_notification, url_prefix='/push_notification')
         pass
 
     def tearDownClass():
         pass
+
+    #register devices token
+    def test_register_devices_token_return_correct(self):
+        user_id = '1'
+        token = 'test_token'
+        register_rv = self.app.put(
+            '/push_notification/users/' + user_id + '/devices', data=dict(token=token),
+            follow_redirects=False)
+
+        register_result = json.loads(str(register_rv.data, 'utf-8'))
+        self.assertEqual(ResultType.REGISTER_DIVICES_TOKEN_SUCCESS,
+                         register_result['result'])
+
+    def test_register_devices_token_return_empty_token_error(self):
+        user_id = '1'
+        token = ''
+        register_rv = self.app.put(
+            '/push_notification/users/' + user_id + '/devices', data=dict(token=token),
+            follow_redirects=False)
+
+        register_result = json.loads(str(register_rv.data, 'utf-8'))
+        self.assertEqual(ResultType.REGISTER_DIVICES_EMPTY_TOKEN_ERROR,
+                         register_result['result'])
 
     def test_push_notification_return_correct(self):
         pass
--- flask_webapp/webapp/push_notification.py
+++ flask_webapp/webapp/push_notification.py
@@ -1,14 +1,29 @@
-from flask import Blueprint, current_app
+from flask import request, jsonify, Blueprint, current_app
 
 
 push_notification = Blueprint('push_notification', __name__,
                 template_folder='templates')
 
+class ResultType:
+    REGISTER_DIVICES_TOKEN_SUCCESS = 'success'
+    REGISTER_DIVICES_EMPTY_TOKEN_ERROR = 'empty_token_error'
 
-@push_notification.route('/devices/<token>', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def push_notification_receipts_verify(token):
-    current_app.logger.debug('push_notification token {0}'.format(token))
-    return 'push_notification token {0}'.format(token)
+@push_notification.route('/users/<user_id>/devices', methods=['PUT', 'DELETE'])
+def register_devices_token(user_id=None):
+    '''
+        註冊 / 刪除 裝置的 token, 而後可依據 token 來將訊息推播到各裝置
+    '''
+
+    if request.method == 'PUT':
+
+        if request.form['token'] == '':
+            return jsonify(result=ResultType.REGISTER_DIVICES_EMPTY_TOKEN_ERROR)
+
+        return jsonify(result=ResultType.REGISTER_DIVICES_TOKEN_SUCCESS)
+
+    elif request.method == 'DELETE':
+        pass
+
 
 
 @push_notification.route('/message', methods=['POST'])
